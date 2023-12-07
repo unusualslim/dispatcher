83a30a5394c30f64f83dd1d592212c8aae2b085a
John Smarr <john.smarr97@gmail.com>
2023-12-07 13:52:50 -0500

    smtp

--- config/environment.rb
+++ config/environment.rb
@@ -3,14 +3,3 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-# Configure ActionMailer to use SendGrid
-ActionMailer::Base.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'], # This is the string literal 'apikey', NOT the ID of your API key
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'dispatcher.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
