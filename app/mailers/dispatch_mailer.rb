49358d7980b74579b136d8a7860078d1c70545de
John Smarr <john.smarr97@gmail.com>
2024-03-20 15:48:26 -0400

    email formatting

--- app/mailers/dispatch_mailer.rb
+++ app/mailers/dispatch_mailer.rb
@@ -2,7 +2,8 @@ class DispatchMailer < ApplicationMailer
     default from: 'no_reply@loadntrucks.com'
 
     def send_dispatch_email(dispatch)
-        
+        @origin_locations = Location.where(location_category_id: 1)
+        @destination_locations = Location.where(location_category_id: 2)
         @user = User.find_by(id: dispatch.driver.id)
         @dispatch = dispatch
         mail(
