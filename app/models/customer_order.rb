1b37c295e0f5789c775e6914dda7a139b6807eda
John Smarr <john.smarr97@gmail.com>
2025-01-10 11:59:27 -0500

    customer order cards update

--- app/models/customer_order.rb
+++ app/models/customer_order.rb
@@ -1,21 +1,24 @@
 class CustomerOrder < ApplicationRecord
   belongs_to :location
   belongs_to :customer, optional: true
-  has_many :dispatch_customer_orders
+
+  has_many :dispatch_customer_orders, dependent: :destroy
   has_many :dispatches, through: :dispatch_customer_orders
+
   has_many :customer_order_products, dependent: :destroy
   has_many :products, through: :customer_order_products
+
   accepts_nested_attributes_for :customer_order_products, allow_destroy: true
+
   attribute :freight_only, :boolean, default: false
   enum order_status: { New: "New", complete: "Complete", deleted: "Deleted" }
+
   PRODUCTS = [ "DEF", "Regular", "Plus", "Super", "Eth-Regular", "Eth-Plus", "Eth-Super", "Reg-E10", "Plus-E10", "Super-E10", "ULS", "Dyed ULS" ]
   scope :unassigned_open_orders, -> { where(order_status: 'open').where(dispatch_id: nil) }
 
   after_create :add_blank_product
 
   def assign_user_and_create_dispatch
-    # Logic to assign user and create a dispatch
-    # Example:
     Dispatch.create(customer_order: self, assigned_user: User.first) # Adjust assignment logic as needed
   end
 
