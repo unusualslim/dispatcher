b57c2097904fe7fcfe971a756bd5033cf41e9e37
John Smarr <john.smarr97@gmail.com>
2026-09-17 16:26:26 -0400

    Preserve current URL params when switching warehouses

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -1,6 +1,6 @@
 class WarehousesController < ApplicationController
   def set
     session[:warehouse_id] = params[:id].presence
-    redirect_back fallback_location: authenticated_root_path
+    redirect_to params[:return_to].presence || authenticated_root_path
   end
 end
